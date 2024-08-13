1e1e7699e37c7ba943fcd5c9437b1e89aa64c0b2
Adam Hádl <adam.hadl87@gmail.com>
2024-08-13 19:45:18 +0200

    Primevue theme Noir vyextrahovan do zvlastniho souboru

--- src/main.js
+++ src/main.js
@@ -1,67 +1,25 @@
 import './assets/main.scss'
 
-import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import PrimeVue from 'primevue/config';
-import { definePreset } from '@primevue/themes';
-import Aura from '@primevue/themes/aura';
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import NoirTheme from './primevue/noirtheme'
 
 const app = createApp(App);
-
-const Noir = definePreset(Aura, {
-    semantic: {
-        primary: {
-            50: '{zinc.50}',
-            100: '{zinc.100}',
-            200: '{zinc.200}',
-            300: '{zinc.300}',
-            400: '{zinc.400}',
-            500: '{zinc.500}',
-            600: '{zinc.600}',
-            700: '{zinc.700}',
-            800: '{zinc.800}',
-            900: '{zinc.900}',
-            950: '{zinc.950}'
-        },
-        colorScheme: {
-            light: {
-                primary: {
-                    color: '{zinc.950}',
-                    inverseColor: '#ffffff',
-                    hoverColor: '{zinc.900}',
-                    activeColor: '{zinc.800}'
-                },
-                highlight: {
-                    background: '{zinc.950}',
-                    focusBackground: '{zinc.700}',
-                    color: '#ffffff',
-                    focusColor: '#ffffff'
-                }
-            },
-            dark: {
-                primary: {
-                    color: '{zinc.50}',
-                    inverseColor: '{zinc.950}',
-                    hoverColor: '{zinc.100}',
-                    activeColor: '{zinc.200}'
-                },
-                highlight: {
-                    background: 'rgba(250, 250, 250, .16)',
-                    focusBackground: 'rgba(250, 250, 250, .24)',
-                    color: 'rgba(255,255,255,.87)',
-                    focusColor: 'rgba(255,255,255,.87)'
-                }
-            }
-        }
-    }
-});
-
+const pinia = createPinia()
 
 app.use(PrimeVue, {
     theme: {
-        preset: Noir
+        preset: NoirTheme,
+        options: {
+            prefix: 'p',
+            darkModeSelector: '',
+            cssLayer: false
+        }
     }
 });
+app.use(pinia)
 app.use(router)
 app.mount('#app')
