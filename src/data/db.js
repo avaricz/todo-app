288282705d5833946de7dcca2536bd97a5852a36
Adam Hádl <adam.hadl87@gmail.com>
2024-08-13 19:47:24 +0200

    db.js opraveny metody aby vracely data. Delete metoda upravena aby necatchovala 204

--- src/data/db.js
+++ src/data/db.js
@@ -5,14 +5,15 @@ export const paths = {
     allProjects: "js6projects/",
     allTasks: "js6tasks/",
     allPersons: "js6persons/",
-    allPositions: "js6positions/"
-
+    allPositions: "js6positions/",
+    allPersonstasks: "js6personstasks/",
+    allPersonstasksById: "js6personstasks?taskid=12",
 }
 
 export const methods = {
     // GET method
      get(path) {
-        const data = fetch(baseURL + path)
+        return fetch(baseURL + path)
         .then(response => {
             if(!response.ok) {
                 throw new Error('Network response wasn\'t ok' + response.statusText)
@@ -20,19 +21,19 @@ export const methods = {
             return response.json()
         })
         .then(data => {
+            console.log("GET: ", data)
             return data.data
         })
         .catch(error => console.log('GET error: ', error))
-
-        return data
     },
     // POST method
     post(path, body) {
-        fetch(baseURL + path, {
+        return fetch(baseURL + path, {
             method: 'POST',
             body: JSON.stringify(body)
         })
         .then(response => {
+            console.log(response)
             if(!response.ok) {
                 throw new Error('Network response wasn\'t ok' + response.statusText)
             }
@@ -43,7 +44,7 @@ export const methods = {
     },
     // PUT method
     put(recordNumber) {
-        fetch(baseURL + allProjects + recordNumber, { 
+        return fetch(baseURL + allProjects + recordNumber, { 
             method: 'PUT',
             body: JSON.stringify({
                 project: 'AH - Project1',
@@ -60,13 +61,18 @@ export const methods = {
         .catch(error => console.log('PUT Error: ', error))
     },
     // DELETE method
-    delete(path, recordNumber) {
-        fetch(baseURL + path + recordNumber, {
+    async delete(path, recordNumber) {
+        return await fetch(baseURL + path + recordNumber, {
             method: 'DELETE',
         })
         .then(response => {
+            console.log(response)
             if(!response.ok) {
                 throw new Error('Network response wasn\'t ok' , + response.statusText)
+            }
+            if(response.status === 204) {
+                console.log("204: smazano")
+                return "item " + recordNumber + " deleted"
             }
             return response.json()
         })
